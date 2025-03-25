be41e9b110d769c12b36550ac776cb4f76fd67ae
harshmalik464 <harshmalik464@gmail.com>
2025-03-25 16:21:19 +0530

    third

--- src/app/sharlik-solutions-landing-page/page.jsx
+++ src/app/sharlik-solutions-landing-page/page.jsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function MainComponent() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
