5673d24ed0653cb9dbd0a58699c7e8d823f41b1f
harshmalik464 <harshmalik464@gmail.com>
2025-03-25 16:18:16 +0530

    second

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
